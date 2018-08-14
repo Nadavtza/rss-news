5c1204bb3681e7e59927ab47d12f628635e76be6
Nadavtza <bscmcrh@gmail.com>
2018-08-14 19:13:01 +0300

    Add async SELECT

--- maven-demo/src/main/java/CassandraConnector.java
+++ maven-demo/src/main/java/CassandraConnector.java
@@ -1,25 +1,30 @@
   
 import com.datastax.driver.core.Cluster;  
 import com.datastax.driver.core.Host;  
-import com.datastax.driver.core.Metadata;  
+import com.datastax.driver.core.Metadata;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.ResultSetFuture;
+import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
 import com.datastax.driver.core.SimpleStatement;
 import com.datastax.driver.core.Statement;
+import com.datastax.driver.core.querybuilder.Insert;
+import com.datastax.driver.core.querybuilder.QueryBuilder;
+import com.datastax.driver.core.schemabuilder.CreateIndex;
 
-import static java.lang.System.out;  
+import static java.lang.System.out;
+
+import java.util.concurrent.ExecutionException;  
   
 /** 
  * Class used for connecting to Cassandra database. 
  */  
 public class CassandraConnector  
 {  
-  
-   private Cluster cluster;  
-  
-  
+   private Cluster cluster;    
    private Session session;  
-  
- 
+
+   
    public void connect(final String node, final int port)  
    {  
       this.cluster = Cluster.builder().addContactPoint(node).withPort(port).build();  
@@ -46,10 +51,65 @@ public class CassandraConnector
       session.close();
    }  
    
-   public void init() {
-	   Statement s = new SimpleStatement("select release_version from system.local");
-	   s.enableTracing();
-	   session.execute(s);
-	   
-   }
+	
+
+	 
+	public void createTable() {
+
+		    String createKS = "CREATE KEYSPACE IF NOT EXISTS news WITH REPLICATION = { 'class': 'SimpleStrategy', 'replication_factor': '2' }";
+	        session.execute(createKS);
+	        
+	        String query = "create table IF NOT EXISTS news.rss2 (id uuid primary key,title text,url text,link text);";
+	        session.execute(query);
+	        
+	        String createIndex = "create index title ON news.rss (title);";
+	      //Executing the query
+	        session.execute(query);
+	        System.out.println("Index created");
+	        
+	             		
+		   
+		   System.out.println("Created new table");  
+	}
+	
+	public void insertNews(News news)  {  		     
+			 try
+			    { 
+			        Insert insert = QueryBuilder.insertInto("news", "rss2")
+			                                    .value("id", news.getId())
+			                                    .value("title", news.getTitle())
+			                                    .value("url", news.getUrl())
+			                                    .value("link", news.getLink());
+			        session.execute(insert.toString());
+			    }
+			    catch (Exception ex)
+			    {
+			        ex.printStackTrace();
+			    }
+	}  
+	
+	
+   
+
+	public void selectUrl(String title)  {  
+		//select
+		Statement selectQuery= QueryBuilder.select().all().from("news","rss2");
+				//.where(QueryBuilder.eq("title",title)).allowFiltering();
+		
+		
+		ResultSetFuture future = session.executeAsync(selectQuery);
+
+		try {
+			ResultSet rs = future.get();
+			for(Row r:rs.all()){
+			    System.out.println(r.toString());
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}	
+	
+
+	}
 }  
--- maven-demo/src/main/java/News.java
+++ maven-demo/src/main/java/News.java
@@ -1,22 +1,43 @@
+import java.util.Date;
 import java.util.UUID;
 
 public class News {
-	private UUID userId;
+	private UUID id;
 	private String title;
-	private String desc;
-	private String url;
 	
-	public News(UUID id, String title, String desc , String url) {
-		this.userId =UUID.randomUUID();
+	private String url;
+	private String link;
+	private Date date;
+	
+ 	
+	public News(Date date,String title ,String url, String link) {
+		this.id =UUID.randomUUID();
 		this.title = title;
-		this.desc = desc;
+		
 		this.url = url;
+		this.link = link;
+		this.date = date ;
 	}
-	public UUID getUserId() {
-		return userId;
+	public Date getDate() {
+		return date;
 	}
-	public void setUserId(UUID userId) {
-		this.userId = userId;
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	
+	
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+	public UUID getId() {
+		return id;
+	}
+	public void setId(UUID id) {
+		this.id = id;
 	}
 	public String getTitle() {
 		return title;
@@ -24,12 +45,7 @@ public class News {
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	public String getDesc() {
-		return desc;
-	}
-	public void setDesc(String desc) {
-		this.desc = desc;
-	}
+
 	public String getUrl() {
 		return url;
 	}
